4a0db1afbc99970b37a40954bc6f197682a73f01
Jonathan Leigh <jonleigh7@gmail.com>
2019-03-30 10:20:46 -0700

    frontend

--- frontend/tyche/src/ETFComponent.js
+++ frontend/tyche/src/ETFComponent.js
@@ -1,26 +1,60 @@
 import React, { Component } from "react";
-import { Button } from "react-bootstrap";
+import { Button, ButtonGroup, ToggleButton } from "react-bootstrap";
 
 export default class ETFComponent extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-    };
-  }
+        this.state = {
+        };
+    }
 
-  render() {
-    return (
-      <div className="ETFComponent">
+    render() {
+        return (
+            <div className="ETFComponent">
 
-        <span className="ETFName"> 
+            <span className="ETFName"> 
             {this.props.etfname}
-        </span> 
+            </span> 
 
 
+            <div className="quantityIndicator"> 
 
-      </div>
-    );
-  }
+                {this.props.quantity}
+                <br /> 
+                {this.props.equity} 
+
+            </div> 
+
+            <div className="d-flex flex-column">
+
+            <ButtonGroup toggle className="mt-3">
+            <ToggleButton type="radio" name="radio" defaultChecked value="1">
+            buy
+            </ToggleButton>
+            <ToggleButton type="radio" name="radio" value="2">
+            Radio
+            </ToggleButton>
+            <ToggleButton type="radio" name="radio" value="3">
+            Radio
+            </ToggleButton>
+            </ButtonGroup>
+            </div>
+
+            <div className="buyField"> 
+            Buy - {this.props.buyCount}
+            </div> 
+
+            <div className="holdFiled"> 
+            Hold - {this.props.holdCount}
+            </div> 
+
+            <div className="sellField"> 
+            Sell - {this.props.sellCount}
+            </div> 
+
+            </div>
+        );
+    }
 }
 
