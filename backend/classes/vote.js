15633b71a8d835d30e526179f78eb196e8b65645
Thomas Lai <thomas990428@me.com>
2019-03-30 10:40:16 -0700

    Add methods

--- backend/classes/vote.js
+++ backend/classes/vote.js
@@ -3,11 +3,6 @@ class vote {
         if(vote !== -1 && vote !== 0 && vote !== 1) throw new RangeError("Value of 'vote' must be either -1, 0, or 1.");
         this.vote = vote;
         this.weight = -1;
-        this.executed = false;
-    }
-
-    execute() {
-
     }
 }
 
