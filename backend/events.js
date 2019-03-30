766729069cd9909017087d9bafbcbf3172d96da0
Thomas Lai <thomas990428@me.com>
2019-03-30 02:10:56 -0700

    Added events

--- backend/events.js
+++ backend/events.js
@@ -2,15 +2,41 @@ const users = require("./users");
 
 const getEvents = (state) => {
     const login = (req, res) => {
-    
+        const user = users.find(user => {
+            return user.username === req.username && user.password === req.password;
+        });
+        res.send({
+            id: user ? user.id : -1
+        });
     }
     
     const vote = (req, res) => {
-    
+        const votes = state[req.fund].votes;
+        const findVote = voteType => voteType.findIndex(user => user === req.user) !== -1;
+        if(!findVote(votes.buy) && !findVote(votes.hold) && !findVotes(votes.sell)) {
+            switch(req.vote) {
+                case -1: 
+                    votes.sell.push(req.user);
+                    break;
+                case 0:
+                    votes.hold.push(req.user);
+                    break;
+                case 1:
+                    votes.buy.push(req.user);
+                    break;
+            }
+            res.send({
+                status: true
+            });
+        } else {
+            res.send({
+                status: false
+            });
+        }
     }
     
     const getstate = (req, res) => {
-    
+        res.send(state);
     }
 
     const events = {
