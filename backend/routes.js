f1c19af8729f6f8c1aedb546534fdcd1c1af2b0b
Thomas Lai <thomas990428@me.com>
2019-03-30 14:20:03 -0700

    fix getstate

--- backend/routes.js
+++ backend/routes.js
@@ -3,7 +3,7 @@ const routes = (app, state) => {
 
     app.post("/login", (req, res) => events.login(req.body, res, state));
     app.post("/vote", (req, res) => events.vote(req.body, res, state));
-    app.post("/getstate", (req, res) => events.login(req.body, res, state));
+    app.post("/getstate", (req, res) => events.getstate(req.body, res, state));
 }
 
 module.exports = routes;
