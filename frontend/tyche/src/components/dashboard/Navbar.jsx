485901367e93f069b5047bd0e985334fb094b3dd
Jonathan Leigh <jonleigh7@gmail.com>
2019-03-31 04:40:30 -0700

    changed Search to Search for Symbol

--- frontend/tyche/src/components/dashboard/Navbar.jsx
+++ frontend/tyche/src/components/dashboard/Navbar.jsx
@@ -5,7 +5,8 @@ class Navbar extends React.Component {
         return (
             <div className="navbar">
                 <img className="logo" src="./logo.png" />
-                <input type="text" className="search" onChange={this.props.onSearch} placeholder="Search" />
+                <input type="text" className="search" onChange={this.props.onSearch} 
+                placeholder="Search by symbol" />
                 <div className="logout" onClick={ this.props.logout }>Sign Out</div>
             </div>
         )
